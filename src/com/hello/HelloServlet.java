eb4d1d4585f998b559b5ba12ffc51d2c5992c25a
manmyung <manmyung@gmail.com>
2015-05-12 11:27:14 +0900

    Complete https://www.youtube.com/watch?v=licQZlIenAk

---
+++ src/com/hello/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by manmyung on 15. 5. 12..
+ */
+@WebServlet(name = "HelloServlet", urlPatterns = {"/a", "/sevlets/hello"})
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, Servlet</h1>");
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -11,6 +11,11 @@
     <title></title>
   </head>
   <body>
-
+  <%
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    out.println("c=" + c);
+  %>
   </body>
 </html>
